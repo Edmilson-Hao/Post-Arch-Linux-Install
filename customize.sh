d46080cc9d8254ac624cfcbf46fdffb93a89528b
Edmilson-Hao <45573538+Edmilson-Hao@users.noreply.github.com>
2018-12-04 09:17:09 -0200

    Update customize.sh
    Dotfiles are downloaded when repository is clone via "git clone", no need to download them with wget.

--- customize.sh
+++ customize.sh
@@ -1,25 +1,9 @@
 #!/usr/bin/env bash
 
-cd /tmp
 #instalando fontes
 echo "Installing Fonts and required apps"
 pacman -S ttf-font-awesome terminus-font htop xterm ranger scrot feh dmenu rofi --noconfirm
 clear
-
-echo "Downloading i3wm config file"
-wget https://raw.githubusercontent.com/Edmilson-Hao/i3GapsConfig/master/config -O config
-clear
-
-echo "Downloading i3status config file"
-wget https://raw.githubusercontent.com/Edmilson-Hao/i3GapsConfig/master/i3status.conf -O i3status.conf
-clear
-
-echo "Downloading .Xdefaults configuration."
-wget https://raw.githubusercontent.com/Edmilson-Hao/i3GapsConfig/master/Xdefaults -O Xdefaults
-clear
-
-echo "Downloading Wallpaper"
-wget https://raw.githubusercontent.com/Edmilson-Hao/i3GapsConfig/master/wallpaper.jpg -O wallpaper.jgp
 
 #If ~/.wallpaper exit then move wallpaper
 if [[ -d ~/.wallpaper ]] ; then
@@ -45,7 +29,10 @@ if [[ -e ~/.Xdefaults ]] ; then
   cp ~/.Xdefaults ~/.Xdefaults.backup
 fi
 
+
 #Move config files
 mv config ~/.i3/config
 mv Xdefaults ~/.Xdefaults
 mv i3status.conf /etc/i3status.conf
+
+cd .. ; rm -rf i3GapsConfig
