df170bad4082a7ad56db3ad4a5af53b62fc96124
Edmilson-Hao <45573538+Edmilson-Hao@users.noreply.github.com>
2018-12-13 18:58:50 -0200

    Update customize.sh
    Added various menus and reworked functions

--- customize.sh
+++ customize.sh
@@ -1,3 +1,5 @@
+#sed -i 's/#Server/Server/' mirrorlist
+
 #!/usr/bin/env bash
 #
 #
@@ -31,59 +33,114 @@
 #SOFTWARE.
 #
 
-
-
-#Test if the script was executed by root
-[[ $(id -u) -ne "0" ]] && { clear ; echo "This script must be executed by root." ; sleep 2 ; exit 1; }
-
-
 ####################	Functions     ####################	
 
-function rank() {
-	clear
-	#Installing pacman-contrib package for some useful scripts
-	echo "Ranking mirros to get the 6 fastest for your current location."
-	pacman -S pacman-contrib --noconfirm
+#Menu
+function mainMenu(){
+cat << EOF
 
-	#Rank the 6 fastest mirrors for current location.
-	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
-	rankmirrors -n 6 /etc/pacman.d/mirrorlist.backup > /etc/pacman.d/mirrorlist
 
+          ________________________________________________________________________
+          |                                                                      |
+          |         (1) - Get mirrors.                                           |
+          |         (2) - Install xorg and drivers.                              |
+          |         (3) - Install i3-gaps and apps.                              |
+          |         (4) - Configure dotfiles.                                    |
+          |         (5) - Create user and configure sudo.                        |
+          |         (6) - Exit.                                                  |
+          |______________________________________________________________________|
+EOF
 }
 
-#Configuring basic system
-function installPackages() {
+function menuMirros(){
+cat << MIRRORLIST
+______________________________________________________________________________________
+|       A                         |                       |          P               |
+|(AU) - Australia                 | (HU) - Hungary        | (PT) - Portugal          |
+|(AT) - Austria                   |        I              |        Q                 |
+|       B                         | (IS) - Iceland        | (QA) - Qatar             |
+|(BD) - Bangladesh                | (IN) - India          |        R                 |
+|(BY) - Belarus                   | (ID) - Indonesia      | (RO) - Romania           |
+|(BE) - Belgium                   | (IR) - Iran           | (RU) - Russia            |
+|(BA) - Bosnia and Herzegovina    | (IE) - Ireland        |        S                 |
+|(BR) - Brazil                    | (IL) - Israel         | (RS) - Serbia            |
+|(BG) - Bulgaria                  | (IT) - Italy          | (SG) - Singapore         |
+|       C                         |        J              | (SK) - Slovakia          |
+|(CA) - Canada                    | (JP) - Japan          | (SI) - Slovenia          |
+|(CL) - Chile                     |        K              | (ZA) - South Africa      |
+|(CN) - China                     | (KZ) - Kazakhstan     | (KR) - South Korea       |
+|(CO) - Colombia                  | (KE) - Kenya          | (ES) - Spain             |
+|(HR) - Croatia                   |        L              | (SE) - Sweden            |
+|(CZ) - Czechia                   | (LV) - Latvia         | (CH) - Switzerland       |
+|       D                         | (LT) - Lithuania      |        T                 |
+|(DK) - Denmark                   | (LU) - Luxembourg     | (TW) - Taiwan            |
+|       E                         |        M              | (TH) - Thailand          |
+|(EC) - Ecuador                   | (MK) - Macedonia      | (TR) - Turkey            |
+|       F                         | (MX) - Mexico         | (UA) - Ukraine           |
+|(FI) - Finland                   |        N              | (GB) - United Kingdom    |
+|(FR) - France                    | (NL) - Netherlands    | (US) - United States     |
+|       G                         | (NC) - New Caledonia  | (VN) - Vietnam           |
+|(GE) - Georgia                   | (NZ) - New Zealand    |                          |
+|(DE) - Germany                   | (NO) - Norway         |                          |
+|(GR) - Greece                    |        P              |                          |
+|       H                         | (PY) - Paraguay       |                          |
+|(HK) - Hong Kong                 | (PH) - Philippines    |                          |
+|(HU) - Hungary                   | (PL) - Poland         |                          |
+|____________________________________________________________________________________|
+
+Select your mirror: 
+MIRRORLIST
+read mirrorOption
+
+curl -o mirrorlist https://www.archlinux.org/mirrorlist/?country=$mirrorOption&protocol=http&protocol=https&ip_version=4
+sleep 1
+}
+
+function getMirrors() {
+	urlOne="https://www.archlinux.org/mirrorlist/?country="
+	urlTwo="&protocol=http&protocol=https&ip_version=4"
+	clear
+	#Backingup mirrorlist
+	cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+	
+	selectMirror
+	sleep 1
+	mv mirrorlist /etc/pacman.d/mirrorlist
+}
+
+
+#Configuring xorg and drivers
+function installXorgDriver() {
 	clear
 
 	#Enable multilib
 	echo "[multilib]" >> /etc/pacman.conf
 	echo "Include = /etc/pacman.d/mirrorlist" >> /etc/pacman.conf
 	
-	#Installing xorg and basic packages
-	echo "Installing xorg and drivers."
-	pacman -Sy xorg xorg-server xorg-fonts-100dpi xorg-xinit xorg-server-devel
-	pacman -Sy alsa-utils network-manager-applet networkmanager wireless-tools wpa-supplicant wpa-actiond dialog linux-headers
-
+	#Installing xorg
+	pacman -Sy xorg-server xorg-fonts-100dpi xorg-xinit xorg-server-devel alsa-utils network-manager-applet networkmanager wireless-tools wpa-supplicant wpa-actiond dialog linux-headers --noconfirm
 
 	#installing intel drivers
-	echo "Installing drivers."
 	[[ $(cat /proc/cpuinfo | grep -i intel) ]] && pacman -S xf86-video-intel intel-ucode xf86-video-vesa lib32-intel-dri lib32-mesa lib32-libgl --noconfirm
 
 	#Install GPU driver. if=NVIDIA 		elif=AMD
 	if [[ $(lspci | grep -i NVIDIA) ]] ; then
 		pacman -S nvidia nvidia-libgl nvidia-utils nvidia-settings opencl-nvidia --noconfirm
 		#&& cp /root/xorg.conf.new /etc/X11/xorg.conf
-		[[ ! $(cat /usr/lib/modprobe.d/nvidia.conf | grep blacklist) ]] && echo "blacklist nouveau" >> /usr/lib/modprobe.d/nvidia.conf
+		[[ ! $(cat /usr/lib/modprobe.d/nvidia.conf | grep -i blacklist) ]] && echo "blacklist nouveau" >> /usr/lib/modprobe.d/nvidia.conf
 	fi
 
 	if [[ $(lspci | grep -i AMD) ]] ; then
 		pacman -S xf86-video-amdgpu --noconfirm
 
 	fi
-	
+
+}
+
+#Installing Window Manager and Apps
+function installApps(){
 	#installing fonts and apps
-	echo "Installing Fonts and required apps"
-	pacman -S noto-fonts ttf-font-awesome terminus-font htop xterm ranger scrot feh rofi rxvt-unicode i3status i3-gaps --noconfirm
+	pacman -S vim noto-fonts ttf-font-awesome terminus-font htop xterm ranger scrot feh rofi rxvt-unicode i3status i3-gaps --noconfirm
 	read	
 	sleep 2
 	clear
@@ -125,33 +182,36 @@ function configWM() {
 	mv i3status.conf /etc/i3status.conf
 }
 
+function createUser(){
+	echo "Enter your user name: "
+	read myUser
+	useradd -m -g wheel -G video,storage,scanner,optical,kvm,input,floppy,disk,audio -s /bin/bash $myUser
+	passwd $myUser
+}
+
 #removing the folder
 function remove() {
 	cd .. ; rm -rf i3GapsConfig
 }
-#Menu
-function installerMenu(){
-	echo "************************************************************************"
-	echo "*         (1) - Rank mirrors.                                          *"
-	echo "*         (2) - Install packages.                                      *"
-	echo "*         (3) - Configure I3-gaps.                                     *"
-	echo "*         (4) - Exit.                                                  *"
-	echo "************************************************************************"
-}
 
 #####################################################################################
+
+#Test if the script was executed by root
+[[ $(id -u) -ne "0" ]] && { clear ; echo "This script must be executed by the root user." ; sleep 2 ; exit 1; }
 
 clear
 option=1
 while [ $option -ne 5 ] ; do
 	clear
-	installerMenu
+	mainMenu
 	read option
 
 	case $option in	
-		1) rank ;;
-		2) installPackages ;;
-		3) configWM ;;
-		4) remove ; break ;;
+		1) getMirrors ;;
+		2) installXorgDriver ;;
+		3) installApps ;;
+		4) configWM ;;
+		5) createUser ;;
+		6) remove ; break ;;
 	esac
 done
