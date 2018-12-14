31e92aba14522bc85e9b188708578c966a82dc11
Edmilson-Hao <45573538+Edmilson-Hao@users.noreply.github.com>
2018-12-14 12:35:54 -0200

    Update remover.sh

--- remover.sh
+++ remover.sh
@@ -1,1 +1,4 @@
+#!/usr/bin/env bash
+
+#This script do only one thing: Remove everything (but not the base) of your system.
 pacman -R $(comm -23 <(pacman -Qq | sort) <((for i in $(pacman -Qqg base); do pactree -ul "$i"; done) | sort -u))
